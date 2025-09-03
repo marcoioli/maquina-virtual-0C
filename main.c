68ae38c44a65c87e3e2b23ff041490a84a6f3a04
Marco Ioli <markoioli123@gmail.com>
2025-09-03 09:38:37 -0300

    Nuevas constantes

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+#define MEMORY_SIZE 16384           // 16 KiB
+#define OP_MOV 0x10
+#define OP_ADD 0x11
+#define OP_SUB 0x12
+#define OP_MUL 0x13
+#define OP_DIV 0x14
+
+
 int main() {
     printf("Prueba commit");
     return 0;
