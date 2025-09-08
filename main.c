720eb357e59daad0e0a59f552c5967e662ba0389
Marco Ioli <markoioli123@gmail.com>
2025-09-08 14:53:27 -0300

    comentarios y correcion op

--- main.c
+++ main.c
@@ -67,10 +67,10 @@ typedef enum {
 } TipoOperando;
 
 
-struct operando {
+typedef struct  {
   TipoOperando tipoOp;
   uint32_t valor; //2 bytes son cod op y el resto son el valor 
-}
+} Operando;
 
 typedef struct {
   uint16_t base; //dirreccion base (2 bytes) // uint16_t 16 bits sin signo
@@ -118,3 +118,29 @@ int main() {
     printf("Prueba commit");
     return 0;
 }
+
+
+//flujo
+/* 
+leer header, guardo base y tamanio de codigo y data segment
+cargo ds y cs
+cs = inicio(leido) offset
+ds = 1?????????
+
+inicializo ip, al principio ip=cs (punteros) 
+leo ip
+
+73 -> 0111 0011 
+Primero 2 bits -> getop1
+Segundos 2 bits -> getop2 
+5 bits menos significativos -> opc -> instrucciones[opc]
+(xxx1 0000) = 10
+(xxx0 0000) = 0
+
+avanzo ip -> ip+=1byte + bytesop1 + bytes op2
+
+si op1 o op2 es memoria
+cargo mar lar mbr
+
+hago la instruccion correspondiente
+*/
