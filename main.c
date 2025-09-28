c46571431e4a788fbd29ad1705b2d5febc78f569
Marco Ioli <markoioli123@gmail.com>
2025-09-28 20:30:31 -0300

    Entrega MV1 2025 0C

--- main.c
+++ main.c
@@ -38,27 +38,3 @@ int main(int argc, char *argv[]) {
 }
 
 
-//flujo
-/* 
-leer header, guardo base y tamanio de codigo y data segment
-cargo ds y cs
-cs = inicio(leido) offset
-ds = 1?????????
-
-inicializo ip, al principio ip=cs (punteros) 
-leo ip
-
-73 -> 0111 0011 
-Primero 2 bits -> getop1
-Segundos 2 bits -> getop2 
-5 bits menos significativos -> opc -> instrucciones[opc]
-(xxx1 0000) = 10
-(xxx0 0000) = 0
-
-avanzo ip -> ip+=1byte + bytesop1 + bytes op2
-
-si op1 o op2 es memoria
-cargo mar lar mbr
-
-hago la instruccion correspondiente
-*/
